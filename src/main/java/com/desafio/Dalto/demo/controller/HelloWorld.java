071beb387d97c95c4dbc7791684248849a438c29
Matheus <Matheusluz32@gmail.com>
2026-04-30 11:53:26 -0300

    criando endpoint HelloWorld

---
+++ src/main/java/com/desafio/Dalto/demo/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.desafio.Dalto.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello World";
+    }
+
+
+}
