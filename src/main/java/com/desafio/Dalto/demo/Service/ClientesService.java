1be03b9b5c85533691ef08de8fd36b3d9f8803d5
Matheus <Matheusluz32@gmail.com>
2026-04-30 17:46:38 -0300

    criando arquivo teste

--- src/main/java/com/desafio/Dalto/demo/Service/ClientesService.java
+++ src/main/java/com/desafio/Dalto/demo/Service/ClientesService.java
@@ -2,36 +2,33 @@ package com.desafio.Dalto.demo.Service;
 
 import com.desafio.Dalto.demo.Banco.Clientes;
 import com.desafio.Dalto.demo.Repository.ClientesRepository;
-import org.springframework.core.annotation.MergedAnnotations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 
-public class Crud {
+public class ClientesService {
     private final ClientesRepository repositorio;
 
-    public Crud(ClientesRepository repository){
+    public ClientesService(ClientesRepository repository){
         this.repositorio = repository;
+    }
+    public Clientes Criar(Clientes cliente) {
+        return repositorio.save(cliente);
+    }
+    public Clientes Buscar(Long id) {
+        return repositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
     }
     public List<Clientes> listar(){
         return repositorio.findAll();
-
-    }
-    public Clientes Criar(Clientes cliente){
-        return repositorio.save(cliente);
-    }
-    public Clientes Buscar(Long id){
-        return repositorio.findById(id)
-                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
     }
     public Clientes Atualizar(Long id, Clientes dados){
         Clientes c = Buscar(id);
         c.setNome(dados.getNome());
         c.setEmail(dados.getEmail());
-        return repositorio.save(new Clientes());
-    }
+        return repositorio.save(c);    }
     public void Excluir(Long id){
         repositorio.deleteById(id);
     }
