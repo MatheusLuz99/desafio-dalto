b97952384bd7ea6fd4cbca1cc30752a48e921b08
Matheus <Matheusluz32@gmail.com>
2026-04-30 15:14:28 -0300

    criando arquivo teste

--- src/main/java/com/desafio/Dalto/demo/controller/Clientes.java
+++ src/main/java/com/desafio/Dalto/demo/controller/Clientes.java
@@ -1,19 +1,15 @@
 package com.desafio.Dalto.demo.controller;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.*;
+
 
 @Entity
 @Table(name = "TBL_CLIENTES")
-
 public class Clientes{
+
         //Onde vai pedir os dados para o usuario!
         @Id            // esse é a chave do usuario
-        @GeneratedValue         //peço para gerar IDS
+        @GeneratedValue(strategy = GenerationType.IDENTITY)         //peço para gerar IDS
 
         private Long Id;
 
@@ -22,26 +18,29 @@ public class Clientes{
         private String email;
         //Cada private é uma Coluna e cada coluna pediu um dado para o usuario!
 
+        public Clientes(){
+        }
+        //Getters
 
         public Long getId() {
             return Id;
         }
-        public String getnome() {
+        public String getNome() {
            return nome;
         }
         public String getemail() {
             return email;
         }// Essa parte do public vai pegar os dados inseridos pelo usuario e retonar!
 
-        public void setEmail (String email){
+        //Setters
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail (String email){
             this.email = email;
         }
-
-            
-
-
-        // @GetMapping
-        //public String sayHello() {
-          //  return "Hello World";
     }
 
