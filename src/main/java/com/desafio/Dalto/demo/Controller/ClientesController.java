685e3c85e040ce8ee4a4ee8fae68367d1a32ce70
Matheus <Matheusluz32@gmail.com>
2026-04-30 17:51:10 -0300

    criando arquivo teste

--- src/main/java/com/desafio/Dalto/demo/Controller/ClientesController.java
+++ src/main/java/com/desafio/Dalto/demo/Controller/ClientesController.java
@@ -1,24 +1,43 @@
 package com.desafio.Dalto.demo.Controller;
 
 import com.desafio.Dalto.demo.Banco.Clientes;
-import com.desafio.Dalto.demo.Repository.ClientesRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.desafio.Dalto.demo.Service.ClientesService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/clientes")
-
 public class ClientesController {
 
-    private final ClientesRepository repository;
-    public ClientesController(ClientesRepository repository) {
-        this.repository = repository;
+    private final ClientesService service;
+
+    public ClientesController(ClientesService service) {
+        this.service = service;
     }
+
     @GetMapping
     public List<Clientes> listar() {
-        return repository.findAll();
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Clientes buscar(@PathVariable Long id) {
+        return service.Buscar(id);
+    }
+
+    @PostMapping
+    public Clientes criar(@RequestBody Clientes cliente) {
+        return service.Criar(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public Clientes atualizar(@PathVariable Long id, @RequestBody Clientes dados) {
+        return service.Atualizar(id, dados);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id) {
+        service.Excluir(id);
     }
 }
